88b45cde3a030dd5a0fdd88f18f3235260a704fb
Negi <lalit.a.negi@accenture.com>
2025-03-21 16:03:42 +0530

    removed error

--- src/components/WeekSelector.tsx
+++ src/components/WeekSelector.tsx
@@ -21,7 +21,7 @@ const WeekSelector: React.FC<WeekSelectorProps> = ({ weeks }) => {
           className={`px-4 py-2 border rounded transition-colors 
             ${selectedWeekId === week_id ? "bg-[#3AAD73] text-white" : "border-[#3AAD73] text-gray-700"}`}
         >
-         <p className="text-black text-xs font-medium font-neris">  Week of {week} </p>
+         <p className="text-black text-xs font-medium font-neris">  {week} </p>
     
         </button>
       ))}
