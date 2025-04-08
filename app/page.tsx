4a10a814e969898c0e66cfc8d5506956fda4fec9
Negi <lalit.a.negi@accenture.com>
2025-04-08 10:56:01 +0530

    loading fix

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ const Login = () => {
     const checkSession = async () => {
       const session = await getSession();
       if (session) {
-        router.push("/"); // Redirect if session exists
+        router.push("/home"); // Redirect if session exists
       } else {
         setLoading(false); // Stop loading once session check is done
       }
